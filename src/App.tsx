8f8c19282de9f5806e64b986df99b56f5acf555b
Long Dao <longdao@studiodao.com>
2025-04-10 15:19:24 -0700

    add ndjson fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ndjsonStream from 'can-ndjson-stream';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+
+  const fetchNdjson = async (): Promise<void> => {
+    const response = await fetch('https://s3.amazonaws.com/io.cribl.c021.takehome/cribl.log');
+    const ndjson = ndjsonStream(response.body);
+    const reader = ndjson.getReader();
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      console.log(value);
+      setList((prevList) => [...prevList, value])
+
+
+    }
+
+    reader.releaseLock();
+  };
+
+  useEffect(() => {
+    fetchNdjson();
+  }, []);
 
   return (
     <>
@@ -19,11 +43,35 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {list?.length}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {list?.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Event</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item, index) => (
+                <>
+                    <tr
+                      key={index}
+
+                    >
+                      <td>{new Date(item._time).toISOString()}</td>
+                      <td>
+                        <code>
+                          {JSON.stringify(item)}
+                        </code>
+                      </td>
+                    </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
